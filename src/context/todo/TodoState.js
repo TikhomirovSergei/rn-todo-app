433059efb344e35651b202c7d66fe0c9bba581d9
TikhomirovSergei <truebelievervalar@gmail.com>
2020-11-29 12:26:57 +0300

    added firebase test database

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,19 +1,30 @@
 import React, { useContext, useReducer } from "react"
 import { Alert } from "react-native"
 import { ScreenContext } from "../screen/screenContext"
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types"
+import { ADD_TODO, CLEAR_ERROR, FETCH_TODOS, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO } from "../types"
 import { TodoContext } from "./todoContext"
 import { todoReducer } from "./todoReducer"
+import { Http } from "../../http"
 
 export const TodoState = ({ children }) => {
     const initialState = {
-        todos: [{ id: "1", title: "Выучить React-Native" }]
+        todos: [],
+        loading: false,
+        error: null
     }
 
     const { changeScreen } = useContext(ScreenContext)
     const [state, dispatch] = useReducer(todoReducer, initialState)
 
-    const addTodo = title => dispatch({ type: ADD_TODO, title })
+    const addTodo = async title => {
+        clearError()
+        try {
+            const data = await Http.post("https://rn-todo-app-a50fd.firebaseio.com/todos.json", { title })
+            dispatch({ type: ADD_TODO, title, id: data.name })
+        } catch (e) {
+            showError("Что-то пошло не так...")
+        }
+    }
 
     const removeTodo = id => {
         const todo = state.todos.find(t => t.id === id)
@@ -27,8 +38,9 @@ export const TodoState = ({ children }) => {
                 }, {
                     text: "Удалить",
                     style: "destructive",
-                    onPress: () => {
+                    onPress: async () => {
                         changeScreen(null)
+                        await Http.get(`https://rn-todo-app-a50fd.firebaseio.com/todos/${id}.json`)
                         dispatch({ type: REMOVE_TODO, id })
                     }
                 }
@@ -37,15 +49,50 @@ export const TodoState = ({ children }) => {
         )
     }
 
-    const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
+    const fetchTodos = async () => {
+        showLoader()
+        clearError()
+        try {
+            const data = await Http.get("https://rn-todo-app-a50fd.firebaseio.com/todos.json")
+            const todos = Object.keys(data).map(key => ({ ...data[key], id: key }))
+            dispatch({ type: FETCH_TODOS, todos })
+        } catch (e) {
+            showError("Что-то пошло не так...")
+            console.log(e)
+        } finally {
+            hideLoader()
+        }
+    }
+
+    const updateTodo = async (id, title) => {
+        clearError()
+        try {
+            await Http.patch(`https://rn-todo-app-a50fd.firebaseio.com/todos/${id}.json`)
+            dispatch({ type: UPDATE_TODO, id, title })
+        } catch (e) {
+            showError("Что-то пошло не так...")
+            console.log(e)
+        }
+    }
+
+    const showLoader = () => dispatch({ type: SHOW_LOADER })
+
+    const hideLoader = () => dispatch({ type: HIDE_LOADER })
+
+    const showError = error => dispatch({ type: SHOW_ERROR, error })
+
+    const clearError = error => dispatch({ type: CLEAR_ERROR })
 
     return (
         <TodoContext.Provider
             value={{
                 todos: state.todos,
+                loading: state.loading,
+                error: state.error,
                 addTodo,
                 removeTodo,
-                updateTodo
+                updateTodo,
+                fetchTodos
             }}>
             {children}
         </TodoContext.Provider>
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,20 +1,69 @@
-import React, { useContext } from "react"
-import { StyleSheet, View, FlatList, Image } from "react-native"
+import React, { useState, useContext, useEffect, useCallback } from "react"
+import { StyleSheet, View, FlatList, Image, Dimensions } from "react-native"
 
 import { AddTodo } from "../components/AddTodo"
 import { Todo } from "../components/Todo"
+import { AppButton } from "../components/ui/AppButton"
+import { AppLoader } from "../components/ui/AppLoader"
+import { AppText } from "../components/ui/AppText"
 import { ScreenContext } from "../context/screen/screenContext"
 import { TodoContext } from "../context/todo/todoContext"
+import { THEME } from "../theme"
 
 export const MainScreen = () => {
-    const { todos, addTodo, removeTodo } = useContext(TodoContext)
+    const { todos, addTodo, removeTodo, fetchTodos, loading, error } = useContext(TodoContext)
     const { changeScreen } = useContext(ScreenContext)
+    const [deviceWidth, setDeviceWidth] = useState(
+        Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+    )
+
+    const loadTodos = useCallback(async () => await fetchTodos(), [fetchTodos])
+
+    useEffect(() => {
+        loadTodos()
+    }, [])
+
+    useEffect(() => {
+        const update = () => {
+            const width =
+                Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+            setDeviceWidth(width)
+        }
+
+        Dimensions.addEventListener('change', update)
+
+        return () => {
+            Dimensions.removeEventListener('change', update)
+        }
+    })
+
+    if (loading) {
+        return <AppLoader />
+    }
+
+    if (error) {
+        return (
+            <View style={styles.center}>
+                <AppText style={styles.error}>
+                    {error}
+                </AppText>
+                <AppButton onPress={loadTodos}>
+                    Повторить
+                </AppButton>
+            </View>
+        )
+    }
 
     let content = (
-        <FlatList
-            keyExtractor={item => item.id}
-            data={todos}
-            renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} onOpen={changeScreen} />} />
+        <View style={{ width: deviceWidth }}>
+            <FlatList
+                keyExtractor={item => item.id.toString()}
+                data={todos}
+                renderItem={({ item }) => (
+                    <Todo todo={item} onRemove={removeTodo} onOpen={changeScreen} />
+                )}
+            />
+        </View>
     )
 
     if (todos.length === 0) {
@@ -46,5 +95,14 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         resizeMode: "contain"
+    },
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    error: {
+        fontSize: 20,
+        color: THEME.DANGER_COLOR
     }
 })
